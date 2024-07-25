da2c18e3f2249454cfa352a1991d2f83afba44d9
RomanPustovyi <roman9pustov@gmail.com>
2024-07-25 05:12:50 +0200

    Add persist app state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
 import { Layout } from './components/Layout'
 import { Todo } from './components/Todo'
+import { store } from './store'
+import { saveToLocalStorage } from './features/todo/utils'
 
 const App = () => {
+	useEffect(() => {
+		store.subscribe(() => {
+			setTimeout(() => {
+				saveToLocalStorage(store.getState())
+			}, 1000)
+		})
+	}, [])
+
 	return (
 		<Layout>
 			<Todo />
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from './features/todo/todoSlice'
+import { loadStateFromLocalStorage } from './features/todo/utils'
 
 export const store = configureStore({
 	reducer: {
 		todoReducer
 	},
+	preloadedState: loadStateFromLocalStorage()
 })
