afb5ee740fe2cc6b32b12920527b3b5dcc03de5d
Roman <71887640+RomanPustovyi@users.noreply.github.com>
2024-07-26 02:38:45 +0200

    Revert "Basic Todo app"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import { useEffect } from 'react'
-import { Layout } from './components/Layout'
-import { Todo } from './components/Todo'
-import { store } from './store'
-import { saveToLocalStorage } from './features/todo/utils'
-
 const App = () => {
-	useEffect(() => {
-		store.subscribe(() => {
-			setTimeout(() => {
-				saveToLocalStorage(store.getState())
-			}, 1000)
-		})
-	}, [])
+  return (
+    <div className="App">
 
-	return (
-		<Layout>
-			<Todo />
-		</Layout>
-	)
+    </div>
+  )
 }
 
 export default App
